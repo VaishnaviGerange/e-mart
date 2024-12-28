0d64fd0395d8c09cf294e94f5c65973780a86975
Vaishnavi Gerange <gerangevaishnavi2003@gmail.com>
2024-12-29 01:01:45 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,23 @@
 // const nextConfig = {};
 
 // export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//     },
+//   };
+  
+//   export default nextConfig;
+  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['fakestoreapi.com'],
+  },
+};
+
+export default nextConfig;
